e582cbf21e89278cf86efa7385dae9ac70393b64
reymond <reymondgomera9@gmail.com>
2021-07-11 15:59:34 +0800

    deploy commit

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const developmentConfig = {
-   user: process.env.PG_USER,
-   password: process.env.PG_PASSWORD,
-   host: process.env.PG_HOST,
-   port: process.env.PG_PORT,
-   database: process.env.PG_DATABASE,
-};
+// const developmentConfig = {
+//    user: process.env.PG_USER,
+//    password: process.env.PG_PASSWORD,
+//    host: process.env.PG_HOST,
+//    port: process.env.PG_PORT,
+//    database: process.env.PG_DATABASE,
+// };
 
-const productionConfig = {
-   connectionString: process.env.DATABASE_URL, // from heruko add-ons, heruko provided the environment variable
-};
+const developmentConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const productionConfig = process.env.DATABASE_URL; // from heruko add-ons, heruko provided the environment variable
 
-// const pool = new Pool(process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig);
-const pool = new Pool(productionConfig);
+const pool = new Pool({
+   connectionString: process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig,
+});
 
 module.exports = pool;
